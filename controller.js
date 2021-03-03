8de36f753290b0004ffac54bcc8def60bf15273e
Muhamad Tajudin Subki <mtajudin98@gmail.com>
2021-03-03 21:49:43 +0700

    update controller show data

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("Aplikasi RestApi berjalan",res)
 };
+
+//show all data mahasiswa
+exports.showAll = function(req, res){
+    connection.query("SELECT * FROM mahasiswa", function(err , rows, field){
+        if(err){
+            connection.log(err);
+        }else{
+           response.ok(rows, res) 
+        }
+    });
+};
